c3199c575d9bea5a601f568ad620ead323cfb61f
akshay-narkar <akshaynarkar1295@gmail.com>
2021-01-24 21:45:34 +0530

    Add validations for forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-    
-    validates :email, uniqueness: true
-    validates :password, uniqueness:true
-    validates :username, uniqueness: true
+  validates :password, presence: { message: 'Needs to be present' },
+                       length: { minimum: 7, message: 'Password should be atleast 7 characters long' }
+  validates :email, presence: { message: 'Needs to be present' },
+                    uniqueness: { message: 'Already taken. Please try a new one' }, length: { minimum: 5 }
+  validates :username, presence: { message: 'Needs to be present' },
+                       uniqueness: { message: 'Already taken. Please try a new one' },
+                       length: { minimum: 2, message: 'Username should be atleast 3 characters long' }
 end
