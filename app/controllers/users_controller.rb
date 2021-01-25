d7c7a9cb1158809774f91fdb271f39344247712d
dibdas <dibyandu08das@gmail.com>
2021-01-25 13:41:10 +0530

    removed commented code from controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # def create
-  #   @user = User.new(username:params[:username],email:params[:email],password:params[:password])
-  #   if @user.save
-  #     redirect_to new_user_path
-  #   else
-  #     render :new
-  #    end
-  # end
-
+  
   def create
     @user = User.new(whitelisted_post_params)
 
